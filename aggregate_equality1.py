2ab44403e8110d1b045de0dc4f9654384943fbbf
Max Ostrowski <max.ostrowski@potassco.com>
2023-04-12 14:56:30 +0200

    cleanup

---
+++ aggregate_equality1.py
@@ -0,0 +1,245 @@
+"""
+ This module replaces bodys with aggregates of the form X = #agg{}
+ and other literals of the form lb < X < ub with the direct replacement
+ lb < #agg{} < ub
+"""
+
+from clingo.ast import (
+    ASTType,
+    Comparison,
+    ComparisonOperator,
+    Guard,
+    Literal,
+    Location,
+    Position,
+    Sign,
+    Transformer,
+)
+
+
+def reverse_comparison(cmp):
+    """reverse clingo.ast.ComparisonOperator"""
+    return {
+        ComparisonOperator.Equal: ComparisonOperator.NotEqual,
+        ComparisonOperator.NotEqual: ComparisonOperator.Equal,
+        ComparisonOperator.GreaterEqual: ComparisonOperator.LessEqual,
+        ComparisonOperator.LessEqual: ComparisonOperator.GreaterEqual,
+        ComparisonOperator.GreaterThan: ComparisonOperator.LessThan,
+        ComparisonOperator.LessThan: ComparisonOperator.GreaterThan,
+    }[cmp]
+
+
+class BodyAggAnalytics:
+    """class to analyze a body aggregate and capture its bounds"""
+
+    def __init__(self, node):
+        assert node.ast_type == ASTType.BodyAggregate
+        self.equal_bound = None
+        self.two_equals = False
+        self.bounds = []  # all non equal bounds as right guards
+
+        if node.left_guard and node.left_guard.ast_type == ASTType.Guard:
+            guard = node.left_guard
+            if (
+                guard.comparison == ComparisonOperator.Equal
+                and guard.term.ast_type == ASTType.Variable
+            ):
+                self.equal_bound = guard.term.name
+            else:
+                self.bounds.append(
+                    guard.update(comparison=reverse_comparison(guard.comparison))
+                )
+
+        if node.right_guard and node.right_guard.ast_type == ASTType.Guard:
+            guard = node.right_guard
+            if (
+                guard.comparison == ComparisonOperator.Equal
+                and guard.term.ast_type == ASTType.Variable
+            ):
+                if self.equal_bound is not None:
+                    self.two_equals = True
+                    return
+                self.equal_bound = guard.term.name
+            else:
+                self.bounds.append(guard)
+
+
+class __ContainsVariable(Transformer):
+    """helper class for function contains_variable"""
+
+    def __init__(self, name):
+        self.__name = name
+        self.contains_variable = False
+
+    def visit_Variable(self, node):
+        if node.name == self.__name:
+            self.contains_variable = True
+        return node
+
+
+def contains_variable(name, stm):
+    """returns true if an AST contains a variable "name" """
+    checker = __ContainsVariable(name)
+    checker.visit(stm)
+    return checker.contains_variable
+
+
+class __ContainsInterval(Transformer):
+    """helper class for function contains_interval"""
+
+    def __init__(self):
+        self.contains_interval = False
+
+    def visit_Interval(self, node):
+        self.contains_interval = True
+        return node
+
+
+def contains_interval(stm):
+    """returns true if an AST contains am variable "clingo.ast.Interval" """
+    checker = __ContainsInterval()
+    checker.visit(stm)
+    return checker.contains_interval
+
+
+class BoundComputer:
+    """class to find all bound restrictions of a variable varname"""
+
+    def __init__(self, varname):
+        self.varname = varname
+        self.too_complicated = False
+        self.bounds = []
+        self.rest = []
+
+    def __create_ordered_comparison(self, lhs, op, rhs):
+        if lhs.ast_type == ASTType.Variable and lhs.name == self.varname:
+            self.bounds.append(Comparison(lhs, [Guard(op, rhs)]))
+        elif rhs.ast_type == ASTType.Variable and rhs.name == self.varname:
+            self.bounds.append(Comparison(rhs, [Guard(reverse_comparison(op), lhs)]))
+        else:
+            self.too_complicated = (
+                True
+                if contains_variable(self.varname, lhs)
+                or contains_variable(self.varname, rhs)
+                else self.too_complicated
+            )
+            pos = Position("<string>", 1, 1)
+            loc = Location(pos, pos)
+            self.rest.append(
+                Literal(loc, Sign.NoSign, Comparison(lhs, [Guard(op, rhs)]))
+            )
+
+    def compute_bounds(self, literal):
+        """
+        compute self.bounds as bounds for varname from a given ast
+        compute self.rest for all non bounds for varname from a given ast
+        sets self.too_complicated to True if AST is too complicated to analyize
+        but still contains variable varname
+        """
+        if not contains_variable(self.varname, literal):
+            self.rest.append(literal)
+            return
+
+        if contains_interval(literal):
+            self.too_complicated = True
+            return
+
+        if (
+            literal.ast_type != ASTType.Literal
+            or literal.atom.ast_type != ASTType.Comparison
+        ):
+            self.too_complicated = True
+            return
+
+        sign = literal.sign
+        comp = literal.atom
+
+        if sign != Sign.NoSign and len(comp.guards) > 1:
+            self.too_complicated = True
+            return
+
+        if (
+            comp.term.ast_type == ASTType.Variable
+            and comp.term.name == self.varname
+            and len(comp.guards) == 1
+        ):
+            if sign == Sign.Negation:
+                newguard = comp.guards[0].update(
+                    comparison=reverse_comparison(comp.guards[0].comparison)
+                )
+                comp = comp.update(guards=[newguard])
+            self.bounds.append(comp)
+        elif not contains_variable(self.varname, comp.term) and len(comp.guards) == 1:
+            guard = comp.guards[0]
+            if (
+                guard.term.ast_type == ASTType.Variable
+                and guard.term.name == self.varname
+            ):
+                newcomparison = (
+                    guard.comparison
+                    if sign != Sign.NoSign
+                    else reverse_comparison(guard.comparison)
+                )
+                comp = Comparison(guard.term, [Guard(newcomparison, comp.term)])
+                self.bounds.append(comp)
+            else:
+                self.too_complicated = True
+                return
+        else:
+            long_comp = []
+            long_comp.append(comp.term)
+            for guard in comp.guards:
+                long_comp.append(guard.comparison)
+                long_comp.append(guard.term)
+            for i in range(0, len(long_comp) - 2, 2):
+                lhs = long_comp[i]
+                op = long_comp[i + 1]
+                rhs = long_comp[i + 2]
+                self.__create_ordered_comparison(lhs, op, rhs)
+        return
+
+
+# TODO: ensure no cycles though rule
+class EqualVariable(Transformer):
+    """
+    replaces bodys with aggregates of the form X = #agg{}
+    and other literals of the form lb < X < ub with the direct replacement
+    lb < #agg{} < ub
+    """
+
+    def visit_Rule(self, node):
+        assert node.ast_type == ASTType.Rule
+        analytics = {}
+        for i, blit in enumerate(node.body):
+            assert blit.ast_type == ASTType.Literal
+            if blit.atom.ast_type == ASTType.BodyAggregate:
+                agg_info = BodyAggAnalytics(blit.atom)
+                if agg_info.equal_bound is not None and not agg_info.two_equals:
+                    analytics[i] = agg_info
+        for i, agg_info in analytics.items():
+            bcomp = BoundComputer(agg_info.equal_bound)
+            for key, blit in enumerate(node.body):
+                if key == i:
+                    continue
+                bcomp.compute_bounds(blit)
+            if not bcomp.too_complicated:
+                bcomp.bounds = [b.guards[0] for b in bcomp.bounds]
+                bcomp.bounds.extend(agg_info.bounds)
+                if len(bcomp.bounds) <= 2:
+                    sign = node.body[i].sign  # currently not used, this is wrong
+                    agg = node.body[i].atom
+                    agg = agg.update(left_guard=None, right_guard=None)
+                    if len(bcomp.bounds) >= 1:
+                        agg = agg.update(
+                            left_guard=bcomp.bounds[0].update(
+                                comparison=reverse_comparison(
+                                    bcomp.bounds[0].comparison
+                                )
+                            )
+                        )
+                    if len(bcomp.bounds) == 2:
+                        agg = agg.update(right_guard=bcomp.bounds[1])
+                    return node.update(
+                        body=[node.body[i].update(atom=agg, sign=sign)] + bcomp.rest
+                    )
+        return node
--- tests/test_equal_variable.py
+++ tests/test_equal_variable.py
@@ -1,13 +1,31 @@
-from clingo.ast import  Transformer, parse_string
-from main import BoundComputer
 import pytest
+from clingo.ast import Transformer, parse_string
+
+from aggregate_equality1 import BoundComputer, EqualVariable
 
 
-class Trans(Transformer):
+class RunEqualVariable(Transformer):
     def __init__(self):
         self.cbounds = set()
         self.crest = set()
         self.too_complicated = False
+
+    def visit_Rule(self, rule):
+        eq = EqualVariable()
+        for node in rule.body:
+            bc.compute_bounds(node)
+            self.cbounds.update([str(b) for b in bc.bounds])
+            self.crest.update([str(b) for b in bc.rest])
+            self.too_complicated = True if bc.too_complicated else self.too_complicated
+        return rule
+
+
+class RunBoundComputer(Transformer):
+    def __init__(self):
+        self.cbounds = set()
+        self.crest = set()
+        self.too_complicated = False
+
     def visit_Rule(self, rule):
         bc = BoundComputer("X")
         for node in rule.body:
@@ -17,33 +35,74 @@ class Trans(Transformer):
             self.too_complicated = True if bc.too_complicated else self.too_complicated
         return rule
 
-@pytest.mark.parametrize("rule, bounds, rest", [
-    (":- X < 2.", ["X < 2"], []),
-    (":- not X < 2.", ["X > 2"], []),
-    (":- X = 2.", ["X = 2"], []),
-    (":- not X = 2.", ["X != 2"], []),
-    (":- X < 2, X > 4.", ["X < 2", "X > 4"], []),
-    (":- 2 < X.", ["X > 2"], []),
-    (":- not 2 < X.", ["X < 2"], []),
-    (":- not 2 < X, X > 4.", ["X < 2", "X > 4"], []),
-    (":- 2 < X < 4.", ["X > 2", "X < 4"], []),
-    (":- 2 < X < 4 < Y < Z + 123.", ["X > 2", "X < 4"], ["4 < Y", "Y < (Z+123)"]),
-    (":- 2 < X, 1 < 4 < Y < Z + 123, f(Y).", ["X > 2"], ["1 < 4 < Y < (Z+123)", "f(Y)"]),
-    ])
+
+@pytest.mark.parametrize(
+    "rule, bounds, rest",
+    [
+        (":- X < 2.", ["X < 2"], []),
+        (":- not X < 2.", ["X > 2"], []),
+        (":- X = 2.", ["X = 2"], []),
+        (":- not X = 2.", ["X != 2"], []),
+        (":- X < 2, X > 4.", ["X < 2", "X > 4"], []),
+        (":- 2 < X.", ["X > 2"], []),
+        (":- not 2 < X.", ["X < 2"], []),
+        (":- not 2 < X, X > 4.", ["X < 2", "X > 4"], []),
+        (":- 2 < X < 4.", ["X > 2", "X < 4"], []),
+        (":- 2 < X < 4 < Y < Z + 123.", ["X > 2", "X < 4"], ["4 < Y", "Y < (Z+123)"]),
+        (
+            ":- 2 < X, 1 < 4 < Y < Z + 123, f(Y).",
+            ["X > 2"],
+            ["1 < 4 < Y < (Z+123)", "f(Y)"],
+        ),
+    ],
+)
 def test_bound_computation(rule, bounds, rest):
-    t = Trans()
+    t = RunBoundComputer()
     parse_string(rule, lambda stm: t(stm))
     assert set(bounds) == t.cbounds
     assert set(rest) == t.crest
     assert not t.too_complicated
 
-@pytest.mark.parametrize("rule", [
-    ":- not 1 < X < 2.",
-    ":- 1 < X+1 < 2.",
-    ":- f(X).",
-    ":- X = 1..7.",
-    ])
+
+@pytest.mark.parametrize(
+    "rule",
+    [
+        ":- not 1 < X < 2.",
+        ":- 1 < X+1 < 2.",
+        ":- f(X).",
+        ":- X = 1..7.",
+    ],
+)
 def test_toocomplicated_bounds(rule):
-    t = Trans()
+    t = RunBoundComputer()
     parse_string(rule, lambda stm: t(stm))
     assert t.too_complicated
+
+
+@pytest.mark.parametrize(
+    "rule, result",
+    [
+        (
+            "#false :- 1 <= #sum { 1,a: a; 1,b: b; 1,c: c } <= 2, X = #sum { 1,e: e; 1,f: f; 1,g: g } 3, X>=2, 5>3, X=Y, 1<=X!=4<5.",
+            "#false :- 1 <= #sum { 1,a: a; 1,b: b; 1,c: c } <= 2; X = #sum { 1,e: e; 1,f: f; 1,g: g } <= 3; X >= 2; 5 > 3; X = Y; 1 <= X != 4 < 5.",
+        ),
+        (
+            "#false :- 1 <= #sum {1,a : a;1,b: b;1,c: c} <= 2, X = #sum {1,e: e;1,f: f;1,g: g} 3, X>=2>1, 5>3.",
+            "#false :- 2 <= #sum { 1,e: e; 1,f: f; 1,g: g } <= 3; 1 <= #sum { 1,a: a; 1,b: b; 1,c: c } <= 2; 2 > 1; 5 > 3.",
+        ),
+        (
+            "#false :- 1 <= #sum {1,a : a;1,b: b;1,c: c} <= 2, not X = #sum {1,e: e;1,f: f;1,g: g} 3, X>=2>1, 5>3.",
+            "#false :- not 2 <= #sum { 1,e: e; 1,f: f; 1,g: g } <= 3; 1 <= #sum { 1,a: a; 1,b: b; 1,c: c } <= 2; 2 > 1; 5 > 3.",
+        ),
+    ],
+)
+def test_equal_variable(rule, result):
+    eq = EqualVariable()
+
+    class RuleVisitor(Transformer):
+        def visit_Rule(self, stm):
+            assert str(stm) == result
+            return stm
+
+    ruler = RuleVisitor()
+    parse_string(rule, lambda stm: ruler(eq(stm)))
